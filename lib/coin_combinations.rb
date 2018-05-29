3c57d3ff31794b908ae39fe5c728b18fe9aa4b37
Joshua Faggion <joshua.faggion@gmail.com>
2018-05-29 00:03:20 -0400

    Set up guidelines for code

---
+++ lib/coin_combinations.rb
@@ -0,0 +1,18 @@
+require('pry')
+
+class Fixnum
+
+  # Set the values for the seperate coins.
+
+  # Set counters for each coin.
+
+  # Run an until loop to each till the coin is higher then amount.
+  # With the amount being subtracted by the coin amount.
+
+  # With the penny use penny % (amount + 1) == 0
+  # This checks to see if amount + 1 equals 0.
+  # And when it does, get to the last part.
+
+  # Then interpolate the coin counters into a string and return it.
+
+end
